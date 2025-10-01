4438a80ec045ef82bf08694d672f13e3352a2e6d
feldt <alcid3srz@gmail.com>
2025-10-01 09:08:40 -0600

    refactor: streamline JWT authorization logic and remove commented code

--- app/middleware/jwt_validations.py
+++ app/middleware/jwt_validations.py
@@ -15,17 +15,16 @@ def authorize(required_claims=None):
         def decorated_wrapper(*args, **kwargs):
             auth_header = request.headers.get('Authorization',"")
             try:
+                if app_config.__OU__ =="":
+                    raise TokenExpired()
+                
                 if not auth_header:
                     raise TokenInvalidAuth()
                 
                 if "Bearer" not in auth_header:
                     raise TokenInvalidAuth()
                 
-                if app_config.__OU__ =="":
-                    raise TokenExpired()
-                
-                token = auth_header.split(" ")[2]
-                payload = jwe().deserialize_compact(token, app_config._KEY_)
+                payload = jwe().deserialize_compact(auth_header.split('Bearer ')[1], app_config._KEY_)
                 data = json.loads(payload['payload'].decode())
                 if required_claims:
                     for k, v in required_claims.items():
@@ -50,17 +49,3 @@ def authorize(required_claims=None):
         return decorated_wrapper
     return jwt_required
     
-    
-# # if __name__ == "__main__":
-#     # Example usage
-#     payload = {
-#     "service": "ATS",
-#     "environment": "development",
-#     "phrase": "vamarela"
-# }
-#     token = create_jwt_token(payload)
-#     print(f"Generated JWT Token: {token}")
-    
-#     decoded_payload = decode_jwt_token(token)
-    
-#     print(f"Decoded Payload: {decoded_payload}")
